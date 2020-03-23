037d4c15b6d442a23c7c30fd6c402c2ade1f6411
bcov77 <bcov@uw.edu>
2020-03-23 11:12:19 -0700

    First commit

---
+++ collect_and_prepare_next.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import getpass
+
+import argparse
+from collections import defaultdict
+
+
+import distutils.spawn
+sys.path.append(os.path.dirname(distutils.spawn.find_executable("silent_tools.py")))
+import silent_tools
+from silent_tools import cmd
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-input_silents", type=str, nargs="+")
+# parser.add_argument("-batch_id_map", type=bool, nargs="+")
+parser.add_argument("-info_files", type=str, nargs="+")
+parser.add_argument("-collected_jobs", type=str, nargs="*", default=[])
+parser.add_argument("-collected_silents", type=str, nargs="*", default=[])
+parser.add_argument("-resubmit_pending_tags", action="store_true")
+parser.add_argument("-user", type=str, default="")
+parser.add_argument("-j", type=int, default=1)
+
+args = parser.parse_args(sys.argv[1:])
+
+
+user = args.user
+if ( user == "" ):
+    user = getpass.getuser()
+
+
+
+all_input_tags = set()
+
+if ( hasattr( os, "all_input_tags" ) ):
+    all_input_tags = os.all_input_tags
+
+else:
+    print("Reading input tags")
+    for input_silent in args.input_silents:
+        print("    " + input_silent)
+        silent_index = silent_tools.get_silent_index( input_silent )
+
+        for tag in silent_index['tags']:
+            all_input_tags.add(tag)
+    os.all_input_tags = all_input_tags
+
+# print("Reading batch ids")
+# id_to_job = {}
+# with open(args.batch_id_map) as f:
+#     for line in f:
+#         line = line.strip()
+#         if (len(line) == 0):
+#             continue
+#         sp = line.split()
+
+#         for idd in sp[:-1]:
+#             id_to_job[idd] = sp[-1]
+
+print("Reading info files")
+tag_to_runnames = defaultdict(list)
+collectable_runnames = set()
+
+if ( hasattr( os, "tag_to_runnames" ) ):
+    tag_to_runnames = os.tag_to_runnames
+else:
+    for info_file in args.info_files:
+        print("    " + info_file)
+
+        job = os.path.basename(info_file)
+        assert( job.endswith(".info") )
+        job = job[:-len(".info")]
+
+        with open(info_file) as f:
+            for line in f:
+                line = line.strip()
+                if ( len(line) == 0):
+                    continue
+                sp = line.split()
+
+                runname = sp[-1]
+                collectable_runnames.add(runname)
+
+                for tag in sp[:-1]:
+                    tag_to_runnames[tag].append(runname)
+    os.tag_to_runnames = tag_to_runnames
+
+
+print("Reading collected jobs")
+collected_runnames = set()
+for collected_job_file in args.collected_jobs:
+    print(collected_job_file)
+    with open(collected_job_file) as f:
+        for line in f:
+            line = line.strip()
+            if (len(line) == 0):
+                continue
+            collected_runnames.add(line)
+
+def parse_silent_file(fname, tags_set):
+    print("    " + fname)
+    silent_index = silent_tools.get_silent_index( fname )
+
+    for tag in silent_index['orig_tags']:
+        tag = tag[:-len("00004_0000001_0")]
+        tags_set.add(tag)
+
+print("Reading collected tags")
+collected_tags = set()
+for input_silent in args.collected_silents:
+    parse_silent_file(input_silent, collected_tags)
+
+
+
+
+print("Getting boinc status")
+if ( hasattr(os, "boinc_q_raw") ):
+    boinc_q_raw = os.boinc_q_raw
+else:
+    boinc_q_raw = cmd("/home/bcov/util/boinc_q_bcov.py -long_names")
+    os.boinc_q_raw = boinc_q_raw
+
+boinc_q_raw = boinc_q_raw.strip().split("\n")
+headers = boinc_q_raw[0].split()
+to_col = {}
+for i, head in enumerate(headers):
+    to_col[head] = i
+
+runname_to_status = {}
+runname_to_fullname = {}
+runname_to_batch = {}
+for line in boinc_q_raw[1:]:
+    line = line.strip()
+    if (len(line) == 0):
+        continue
+    sp = line.split()
+    status = sp[to_col["S"]]
+    runname = sp[to_col["NAME"]]
+
+    runname = runname.replace("_SAVE_ALL_OUT", "")
+
+    runname_to_fullname[runname] = sp[to_col["NAME"]]
+    runname_to_batch[runname] = sp[to_col["ID"]].split(".")[0]
+
+    if ( int(sp[to_col["DECOYS"]]) == 0 ):
+        status = "N"
+
+    runname_to_status[runname] = status
+
+
+# now we work on derived products
+
+to_collect = []
+for runname in collectable_runnames:
+    if ( runname in collected_runnames ):
+        continue
+    if ( runname not in runname_to_status ):
+        print("Missing job?: %s"%runname)
+        continue
+    if ( runname_to_status[runname] == "C" ):
+        to_collect.append(runname)
+
+# to_collect = to_collect[:10]
+
+all_paths = []
+for runname in to_collect:
+    fullname = runname_to_fullname[runname]
+    prefix = fullname[:8]
+    batch = runname_to_batch[runname]
+
+    has_all = ".all" if fullname != runname else ""
+
+    path = "/projects/boinc-results/%s/%s_%s_0%s.out.bz2"%(
+                prefix, fullname, batch, has_all)
+    all_paths.append(path)
+
+print("Collecting %i results"%len(all_paths))
+
+with open("tmp", "w") as f:
+    for path in all_paths:
+        f.write("%s\n"%(path))
+
+print(cmd("cat tmp | parallel -j %i --xargs bzcat > tmp.silent"%args.j))
+os.remove("tmp")
+
+print("Removing corrupt models")
+print(cmd("silentdropcorruptmodels tmp.silent > tmp2.silent"))
+os.remove("tmp.silent")
+os.remove("tmp.silent.idx")
+
+print("Renaming duplicate names")
+print(cmd("silentls tmp2.silent | silentrename tmp2.silent > collected.silent"))
+os.remove("tmp2.silent")
+os.remove("tmp2.silent.idx")
+
+
+print("Reading new tags")
+newly_collected_tags = set()
+parse_silent_file("collected.silent", newly_collected_tags)
+
+print("Found %i new tags"%(len(newly_collected_tags - collected_tags)))
+
+print("Determining tags that need to be resubmitted")
+missing_tags = all_input_tags - newly_collected_tags - collected_tags
+
+if ( args.resubmit_pending_tags ):
+    tags_to_submit = missing_tags
+else:
+    tags_to_submit = set()
+    for tag in missing_tags:
+        runnames = tag_to_runnames[tag]
+        is_pending = False
+        for runname in runnames:
+            if ( runname not in runname_to_status ):
+                # print("Missing job?: %s")
+                continue
+            if ( runname_to_status[runname] in "RI" ):
+                is_pending = True
+
+        if ( not is_pending ):
+            tags_to_submit.add(tag)
+
+
+with open("to_resubmit.list", "w") as f:
+    for tag in tags_to_submit:
+        f.write("%s\n"%tag)
+
+with open("collected.list", "w") as f:
+    for runname in to_collect:
+        f.write("%s\n"%runname)
+
+print("Need to submit %i more tags"%len(tags_to_submit))
+
+
+
+
---
+++ design_on_boinc.py
@@ -0,0 +1,453 @@
+#!/usr/bin/env python
+import os, sys
+import string
+import numpy as np
+# from numpy import random
+import re
+# import pyrosetta
+# from pyrosetta import *
+# from pyrosetta.rosetta import *
+import pandas as pd
+import distutils.spawn
+sys.path.append(os.path.dirname(distutils.spawn.find_executable("silent_tools.py")))
+import silent_tools
+from silent_tools import eprint
+
+from argparse import ArgumentParser
+
+from collections import defaultdict
+import time
+import argparse
+import itertools
+import subprocess
+
+from multiprocessing import Pool
+import multiprocessing as mp
+
+
+# Run these in bash to setup a working environment
+#  source activate /software/conda/envs/tensorflow-testing (for numpy)
+#  source /software/pyrosetta3.6/setup.sh  (for pyrosetta)
+#
+# This script generates blueprint, h-bond constraints, and Rosetta script xml files
+# along with various utility scripts for running a local test, submitting jobs to
+# R@h and getting results. This serves as an example and is not a production script.
+#
+# David K
+
+# 
+# Nate has edited this script so that it can take either a list of pdbs or a silent file, an xml file, flags file, number of jobs to be run, and pdbs per job
+# It then writes this to a boinc submission script for the job specified by the pdbs and xml
+# 
+# Nate B
+
+# 
+# 
+# Read in arguments and determine what kind of files we are working with
+# 
+# 
+
+def cmd(command, wait=True, print_output=False):
+
+    the_command = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    if (not wait):
+        return
+    the_stuff = the_command.communicate()
+    output = str(the_stuff[0]) + str(the_stuff[1])
+    if ( print_output ):
+        print(output)
+    return output
+
+
+if (len(sys.argv) == 1):
+    eprint("")
+    eprint('This script prepares your design jobs to run on Rosetta at Home')
+    eprint("Arguments:")
+    eprint(" -run_name          : optional, the name that you want to give to this whole job")
+    eprint(" -xml               : the xml file to use, can be global or local path")
+    eprint(" -flags             : the flags file to use, can be global or local path")
+    eprint(" -pdbs_per_job      : optional, the number of structures to submit per boinc job")
+    eprint(" -j                 : optional, how many cores you want this script to run on")
+    eprint(" -in:file:silent    : the silent file of structures to design")
+    eprint(" -extra_files       : space separated list of extra files to add to run")
+    eprint(" -per_pdb_files     : score file format (tag last column) of extra files for each pdb")
+    eprint(" -add_pdb_ids       : Add pdb ID to REMARK and script_vars")
+    eprint("                             or")
+    eprint(" list of pdbs       : this script will also read in a list of pdbs if not given a silent file")
+    sys.exit(1)
+
+parser = ArgumentParser()
+parser.add_argument("-run_name", type=str, default="design_on_boinc")
+parser.add_argument("-xml", type=str, default="")
+parser.add_argument("-flags", type=str, default="")
+parser.add_argument("-pdbs_per_job", type=int, default=1)
+parser.add_argument("-in:file:silent", type=str, default="")
+parser.add_argument("-j", type=int, default=1)
+parser.add_argument("-extra_files", type=str, nargs="*", default=[])
+parser.add_argument("-per_pdb_files", type=str, default="")
+parser.add_argument("-add_pdb_ids", action="store_true")
+parser.add_argument("-priority", type=int, default=0)
+parser.add_argument('pdbs', type=str, help="input pdb", nargs="*")
+
+args = parser.parse_args(sys.argv[1:])
+
+run_filename = args.run_name
+pdbs = args.pdbs
+silent = args.__getattribute__("in:file:silent")
+xml_filename = args.xml
+flags_filename = args.flags
+n_cores = args.j
+pdbs_per_file = args.pdbs_per_job
+per_pdb_files = args.per_pdb_files
+add_pdb_ids = args.add_pdb_ids
+
+if (xml_filename == '' or flags_filename == ''):
+    sys.exit("This script needs both an xml file and a flags file")
+
+using_silent = False
+if (len(pdbs) == 0):
+    using_silent = True
+    if (silent == ''):
+        sys.exit("This script needs either a list of pdb files or a silent file")
+
+extra_locals = []
+for file in args.extra_files:
+    if ( not os.path.exists(file) ):
+        sys.exit("%s doesn't exist"%file)
+extra_files = " ".join(args.extra_files)
+
+per_pdb_files_dict = defaultdict(list)
+if ( per_pdb_files != "" ):
+    with open(per_pdb_files) as f:
+        for line in f:
+            line = line.strip()
+            if (len(line) == 0):
+                continue
+            sp = line.split()
+            tag = sp[-1]
+
+            # don't check for existence here as there might be a lot...
+            for file in sp[:-1]:
+                per_pdb_files_dict[tag].append(file)
+
+if ( not os.path.exists("jobs") ):
+    os.mkdir("jobs")
+cmd("move_to_scratch jobs")
+
+# 
+# 
+# 
+# Functions
+# 
+# 
+# 
+
+
+# This takes a list of pdbs and writes them to all_structs.silent
+def deal_with_pdbs( list_o_pdbs ):
+    pdbs_string = ' '.join( list_o_pdbs )
+    cmd( '/home/nrbennet/software/silent_tools/silentfrompdbsparallel ' + pdbs_string + ' > all_structs.silent' )
+
+def ensure_silent_is_binary( filename ):
+    # could take this out and just read line in the file until I got to a new struct (would be faster)
+    # Other than that this function works as intended
+    # tags = cmd( '/home/nrbennet/software/silent_tools/silentls ' + filename, True )
+    silent_index = silent_tools.get_silent_index( filename )
+    tags = silent_index['tags']
+    sample = tags[0]
+    # if ( sample not in silent_index['index'] ):
+    #         eprint("silentslice: Unable to find tag: %s"%sample)
+    #         sys.exit("Something has gone seriously wrong")
+    with open( filename ) as total_file:
+        structure = silent_tools.get_silent_structure_file_open( total_file, silent_index, sample )
+        for line in structure:
+            if line[0] in 'ELH':
+                return filename
+
+    # cmd( 'touch binary.silent', False)
+    # Do I need the extra res flag? -- no
+    print("Converting non-binary silent to binary silent")
+
+    command = 'score_jd2 -in:file:silent %s -out:file:silent binary.silent -out:file:silent_struct_type binary 1>&2'%filename
+
+    cmd( command, True )
+    return 'binary.silent'
+
+
+input_scores_re = re.compile(r"\nREMARK[^\n]+_input_score")
+id_re = re.compile(r"\nREMARK[^\n]+ID[^\n]+")
+
+def make_run( xml_filename, runname, i, pdbs_per_file, silent_index ):
+   
+    # silent_index = silent_tools.get_silent_index( all_silent_filename )
+
+    local_extra_files = ""
+    local_extra_files_delete = ""
+    extra_flags = ""
+    id_vars = []
+    extra_job_files = ""
+
+    with open(all_silent_filename) as sf:
+        with open( '%s.silent'%runname, 'w' ) as target:
+        # with open( 'in.silent', 'w' ) as target:
+            target.write( silent_tools.silent_header(silent_index) )
+
+            structures = silent_tools.get_silent_structures_true_slice( sf, 
+                        silent_index, i, i+pdbs_per_file, True )
+            tags = silent_index['tags'][i:i+pdbs_per_file]
+
+            for i, pair in enumerate(zip(structures, tags)):
+                s, t = pair
+
+                first_n = s.find("\n")
+
+                # this is a hack to allow variable length backbones on boinc
+                #   specifically it tricks
+                #   /projects/boinc/workspace/boinc/sched/validate_util.cpp
+                #   on line 692 into skipping the coord_check
+                if ( input_scores_re.search( s ) is None ):
+                    s = s[:first_n+1] + "REMARK _input_score 0\n" + s[first_n+1:]
+
+                if ( add_pdb_ids ):
+                    s = id_re.sub("\n", s)
+                    s = s[:first_n+1] + "REMARK ID %s\n"%(t) + s[first_n+1:]
+                    id_vars.append("id%03i=%s"%(i, t))
+
+                for item in per_pdb_files_dict[t]:
+                    local_extra_files += " " + item
+
+
+                target.write( s )
+
+    zip_name = "jobs/" + runname + ".zip"
+    extra_job_files += ", " + zip_name
+
+    if ( len(id_vars) > 0 ):
+        temp = "jobs/%s.flags"%runname
+        extra_job_files += ", " + temp
+        with open(temp, "w") as f:
+            f.write("-script_vars " + " ".join(id_vars) + "\n")
+
+        extra_flags += " @%s.flags"%runname
+
+
+    zip_command = ("zip -rj " + zip_name + " " + xml_filename + ' %s.silent %s;'%(runname, extra_files + local_extra_files)
+        + " rm %s.silent"%(runname))
+
+    # os.system("zip -r " + runname + ".zip " + xml_filename + ' in.silent')
+    # os.remove( '%s.silent'%runname )
+
+    return extra_flags, zip_command, tags, extra_job_files
+
+
+
+cores_available = os.getenv('SLURM_CPUS_ON_NODE') #cmd( 'echo $SLURM_CPUS_ON_NODE', True )
+if cores_available is None:
+    cores_available = mp.cpu_count()
+else:
+    cores_available = int( cores_available )
+
+if ( n_cores > cores_available ):
+    print( 'There are not %i cores available'%( n_cores ) )
+    n_cores = cores_available
+print( 'Using %i cores'%n_cores )
+
+# this is how many commands have been run
+init_count = mp.Value('i', 0)
+
+def init_thread(counter_in):
+    global counter
+    counter = counter_in
+
+def cmd_thread(*args, **kwargs):
+    global counter
+    counter.value += 1
+    cmd(*args, **kwargs)
+
+
+# do this early to avoid sharing the silent_index
+pool = Pool( n_cores, initializer=init_thread, initargs=(init_count,) )
+
+
+
+# 
+# 
+# 
+# Here is where it all gets put together and called
+# 
+# 
+# 
+
+assert( os.path.exists( flags_filename ) )
+assert( os.path.exists( xml_filename ) )
+
+
+total_flags_filename = os.path.realpath( flags_filename )
+
+local_xml = os.path.basename(xml_filename) #cmd( 'basename %s'%xml_filename, True )
+local_flag = os.path.basename(flags_filename) #cmd( 'basename %s'%flags_filename, True )
+
+
+with open(xml_filename) as f:
+    full_xml = f.read()
+if ( "PoseComment" in full_xml and not args.add_pdb_ids):
+    sys.exit("You probably forgot to specify -add_pdb_ids to commandline")
+
+if ( "/home" in full_xml ):
+    sys.exit("You specified a full path in your xml which is probably wrong")
+
+if ( "~/" in full_xml ):
+    sys.exit("\"~/\" appeared in your xml which is probably wrong")
+
+if ( not re.search(r'="[^"]*/[^"]+/', full_xml) is None ):
+    sys.exit("Found a path in your xml which is probably wrong")
+        
+
+# total_xml_filename = os.path.realpath( xml_filename )
+# scriptpath = os.path.dirname(os.path.realpath(__file__))
+
+all_silent_filename = ''
+if not using_silent:
+    deal_with_pdbs( pdbs )
+    all_silent_filename = all_structs.silent
+else:
+    all_silent_filename = silent
+    # all_silent_filename = ensure_silent_is_binary( silent ) # my silent index takes 3 minutes to open...
+
+print("Loading silent index")
+silent_index = silent_tools.get_silent_index( all_silent_filename )
+
+runnames = set()
+tags = silent_index['tags'] #cmd( '/home/nrbennet/software/silent_tools/silentls ' + all_silent_filename, True )
+num_tags = len(tags) #int(cmd( '/home/nrbennet/software/silent_tools/silentls %s | wc -l'%all_silent_filename, True ))
+
+
+i = 0
+
+run_extra_flags = {}
+run_extra_files = {}
+
+# doing it this way to avoid swear words
+alpha = list(string.ascii_lowercase)
+number = list(string.digits)
+def random_name():
+    # there has to be a better way...
+    return ( np.random.choice(number, 1).item() + "".join(np.random.choice(alpha, 2))
+            + np.random.choice(number, 1).item() + "".join(np.random.choice(alpha, 2))
+            + np.random.choice(number, 1).item() + np.random.choice(alpha, 1).item()
+            )
+
+print("here")
+# This loop splits up tags in the order that they appear in the silent file
+# it shamelessly steals all of that logic from bcov's silentsplitshuf
+scheduled = 0
+with open(run_filename + ".info", "w") as f:
+    while i < num_tags:
+
+        # wouldn't want to accidentally fill up your home dir with 1M files and go over the 10M file limit...
+        if ( scheduled - init_count.value > n_cores * 10 ):
+            time.sleep(0.1)
+            continue
+
+        if i + pdbs_per_file > num_tags:
+            pdbs_per_file = num_tags - i
+        runname = random_name()
+
+        while ( runname in runnames ):
+            runname = random_name()
+
+        runname += "_" + run_filename
+
+        runnames.add(runname)
+
+        these_extra_flags, zip_command, tags, these_extra_files = make_run( xml_filename, runname, i, pdbs_per_file, silent_index )
+
+        run_extra_flags[runname] = these_extra_flags
+        run_extra_files[runname] = these_extra_files
+
+        # cmd(zip_command, print_output=True)
+        pool.apply_async( cmd_thread, args = ( zip_command, True, True ) )
+        scheduled += 1
+
+        i += pdbs_per_file
+
+        f.write(" ".join(tags) + " %s\n"%runname )
+
+
+# This closes the pool and allows all the processes to complete before proceeding in
+# the program
+pool.close()
+pool.join()
+
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+for ichunk, chunk in enumerate(chunks(list(runnames), 30000)):
+    # Here is where I actually write the boinc file
+    with open( run_filename + '_%i.boinc'%ichunk , 'w' ) as f:
+        for i in chunk:
+            f.write("application = rosetta\npriority = %i\n\n"%priority)
+            # f.write("name = %s\n"%run_filename)
+            f.write("name = %s_SAVE_ALL_OUT\n"%i)
+            f.write("description = %s\n"%i)
+            f.write("inputfiles = %s%s\n"%(total_flags_filename, run_extra_files[i]))
+            f.write("arguments = -run:protocol jd2_scripting -parser:protocol " + xml_filename + 
+                # " -out:prefix " + i + "_" +
+                " -database minirosetta_database @" + 
+                local_flag + " -in:file:silent " + i + ".silent -in:file:silent_struct_type binary -silent_gz -mute all" +
+                " -out:file:silent_struct_type binary -out:file:silent default.out -in:file:boinc_wu_zip " +
+                i + ".zip" + 
+                run_extra_flags[i] + 
+                "\n")
+            
+            f.write("resultfiles = default.out.gz\n")
+            # May want to change this later but for now I'm leaving it hard-coded as 1
+            f.write("queue = 1\n")
+
+
+num_chunks = ichunk+1
+
+# 
+# 
+# 
+# Writing convenience scripts
+# 
+# 
+# 
+
+
+# create test shell script
+f = open( run_filename + '.test', 'w')
+f.write('/projects/boinc/bin/run_test_rah.pl ' + run_filename + '_0.boinc')
+f.close()
+
+# create boinc submit shell script
+f = open( run_filename + '.submit', 'w')
+for ichunk in range(num_chunks):
+    f.write("/projects/boinc/bin/boinc_submit " + run_filename + "_%i.boinc\n"%(ichunk))
+f.write("\n# Useful scripts (run with - for usage)\n")
+f.write("#   /projects/boinc/bin/boinc_q <batchid>\n")
+f.write("#   /projects/boinc/bin/boinc_resize -size <size> <batchid>\n")
+f.write("#   /projects/boinc/bin/boinc_rm <batchid>\n")
+f.write("# Status page\n")
+f.write("#   https://boinc.bakerlab.org/queue (username: boinc password: results)\n")
+f.close()
+
+# create results retrieval shell script
+# probably will need to modify this -NRB
+
+# f = open( run_filename + '.results', 'w')
+# f.write("cp /projects/boinc-results/"+job_name[:8]+"/"+job_name+"*.all.out.bz2 ./\n")
+# f.write("bunzip2 "+job_name+"*.all.out.bz2\n")
+# f.write("\n# Extract silent file for pdbs\n/software/rosetta/latest/bin/extract_pdbs -in:file:silent_struct_type binary -silent_read_through_errors -in:file:silent "+job_name+"*.all.out\n")
+# f.write("\n# Here is an example of running various design selection filters. This will also extract the pdbs.\n");
+# f.write("/home/dekim/src/design_on_boinc/Rosetta_dev/main/source/bin/rosetta_scripts.hdf5.linuxgccrelease -in:file:silent "+job_name+"*.all.out -parser:protocol 
+#/home/dekim/src/design_on_boinc/inputfiles/filter.xml -beta @/home/dekim/src/design_on_boinc/inputfiles/filter_flags -out:file:scorefile "+job_name+".sc\n")
+# f.close()
+
+
+
